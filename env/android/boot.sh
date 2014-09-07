acb7bcf27cdae60b21c4c96770b7639a46af39d4
Zach Latta <zach@zachlatta.com>
2014-09-07 07:56:21 -0700

    Fix boot.sh.

--- env/android/boot.sh
+++ env/android/boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/app/im-vnc/docker/pre_build.sh                                                 
+/usr/local/im-vnc/docker/pre_build.sh
 DISPLAY=:0.0 emulator -avd test -no-skin -no-audio -port 5554 &
-/app/im-vnc/docker/post_build.sh                                                
+/usr/local/im-vnc/docker/post_build.sh                                                
 while true; do sleep 2; done
