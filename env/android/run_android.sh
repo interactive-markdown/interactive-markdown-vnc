1b7b028b12594d0dfef351c1a7aae3dd242fa166
XiZhao <kwang1imsa@gmail.com>
2014-09-07 03:15:41 -0700

    android

--- env/android/run_android.sh
+++ env/android/run_android.sh
@@ -1,6 +1,6 @@
 /bin/bash /usr/local/im-vnc/docker/pre_build.sh
 
-#run something on display 0.0
+DISPLAY=:0.0 /usr/local/im-vnc/env/android/android-sdk/tools/emulator -avd emu_default &
 
 /bin/bash /usr/local/im-vnc/docker/post_build.sh
 while true; do sleep 1; done
